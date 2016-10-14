c069fdca5cf78c1f2b012679a597e88444759ef6
gvergnaud <gabriel.vergnaud@hetic.net>
2016-10-14 10:43:42 -0400

    add stream support for files

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const ss = require('socket.io-stream')
+
 
 
 io.on('connection', socket => {
@@ -8,8 +10,15 @@ io.on('connection', socket => {
     socket.broadcast.emit('text_copy', data)
   })
 
-  socket.on('file_copy', data => {
-    socket.broadcast.emit('file_copy', data)
+  ss(socket).on('file_copy', (stream, data) => {
+   Object.keys(io.sockets.connected)
+    .map(k => io.sockets.connected[k])
+    .filter(connectedSocket => socket.id !== connectedSocket.id)
+    .forEach(connectedSocket => {
+      const emitStream = ss.createStream()
+      ss(connectedSocket).emit('file_copy', emitStream, data)
+      stream.pipe(emitStream)
+    })
   })
 })
 
